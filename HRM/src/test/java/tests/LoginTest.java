9b2a777df4d0c890f5d216ad8f2245cc259e39af
Rajat Narang <rajatnarang875@gmail.com>
2026-06-06 00:20:50 +0530

    first commit on new branch updated kk

--- HRM/src/test/java/tests/LoginTest.java
+++ HRM/src/test/java/tests/LoginTest.java
@@ -1,5 +1,7 @@
 package tests;
-
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 import base.BaseTest;
